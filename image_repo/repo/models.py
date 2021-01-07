095d4c4ca6bfb17ab99994e1049de22339e09a3f
Arrun Patkunasingam <patkunaa@mcmaster.ca>
2021-01-07 15:39:46 -0500

    added user field to Image model

--- image_repo/repo/models.py
+++ image_repo/repo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -10,6 +11,8 @@ class Image(models.Model):
     tags = models.CharField(max_length=500, null=True, blank=True)
     vision_tags = models.CharField(max_length=500, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
+    uploader = models.ForeignKey(
+        User, null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
